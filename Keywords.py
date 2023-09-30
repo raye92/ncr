f8e36117c14c2fa52f7cbd1ba3b4ce79994f703e
Ray Chen <145238038+raye92@users.noreply.github.com>
2023-09-29 22:53:39 -0400

    Add files via upload
    Update to handle inaccessible URLs

--- Keywords.py
+++ Keywords.py
@@ -15,21 +15,24 @@ def request(url):
         return None
 
 def extract_keywords(web_content):
-        soup = BeautifulSoup(web_content, 'html.parser')
-        #turns the html into a string of words
-        texts = soup.get_text()
+        if web_content is not None:
+            soup = BeautifulSoup(web_content, 'html.parser')
+            #turns the html into a string of words
+            texts = soup.get_text()
 
-        #changes text - tokenizes the string into a list of words
-        text = word_tokenize(texts)
+            #changes text - tokenizes the string into a list of words
+            text = word_tokenize(texts)
 
-        #removing stopwords like an, a, etc
-        stopword = set(stopwords.words('english'))
-        filtered_text = [word.lower() for word in text if word.isalnum() and word not in stopword]
+            #removing stopwords like an, a, etc
+            stopword = set(stopwords.words('english'))
+            filtered_text = [word.lower() for word in text if word.isalnum() and word not in stopword]
 
-        print('\nTotal Word Count: ', len(filtered_text))
+            print('\nTotal Word Count: ', len(filtered_text))
 
-        freq_dist = FreqDist(filtered_text)
-        print('\nTop 10 Keywords: ', freq_dist.most_common(10))
+            freq_dist = FreqDist(filtered_text)
+            print('\nTop 10 Keywords: ', freq_dist.most_common(10))
+        else:
+            print("Web content is unaccessible/ empty.")
 
 link = input("Enter url for keyword analysis:")
 extract_keywords(request(link))
